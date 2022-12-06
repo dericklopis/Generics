12aa143555adf00b6dd66cf4815a4672bae9f0b2
dericklopis <stanforever08@gmail.com>
2022-12-06 17:26:14 +0530

    Added:Generics Program

---
+++ Generics/FindMaxNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class FindMaxNum
+    {
+        public static void MaxValue(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater");
+            }
+            else
+                Console.WriteLine("All The Numbers are Equal");
+        }
+    }
+}
